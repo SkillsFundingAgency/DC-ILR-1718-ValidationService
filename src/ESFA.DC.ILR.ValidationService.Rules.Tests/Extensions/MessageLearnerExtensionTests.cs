bc8e175e1409d046bdc857e44d08d46807cf4657
Mike Childs <mike.childs@education.gov.uk>
2018-01-16 14:57:02 +0000

    Extension Methods and Extension Method Unit Tests Migrated

---
+++ src/ESFA.DC.ILR.ValidationService.Rules.Tests/Extensions/MessageLearnerExtensionTests.cs
@@ -0,0 +1,110 @@
+﻿using ESFA.DC.ILR.Model;
+using ESFA.DC.ILR.ValidationService.Rules.Extensions;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Tests.Extensions
+{
+    public class MessageLearnerExtensionTests
+    {
+        [Fact]
+        public void EarliestLearningDeliveryLearnStartDateFor_NullLearningDelivery()
+        {
+            var learner = new MessageLearner();
+
+            learner.LearningDelivery = null;
+
+            learner.EarliestLearningDeliveryLearnStartDateFor(1, 1, 1, 1).Should().BeNull();
+        }
+
+        [Fact]
+        public void EarliestLearningDeliveryLearnStartDateFor_NoMatch()
+        {
+            var learner = new MessageLearner()
+            {
+                LearningDelivery = new MessageLearnerLearningDelivery[]
+                {
+                    new MessageLearnerLearningDelivery()
+                    {
+                        AimType = 1,
+                        ProgType = 1,
+                        FworkCode = 1,
+                        PwayCode = 1,
+                    }
+                }
+            };
+
+            learner.EarliestLearningDeliveryLearnStartDateFor(1, 1, 1, 2).Should().BeNull();
+        }
+
+        [Fact]
+        public void EarliestLearningDeliveryLearnStartDateFor_SingleMatch()
+        {
+            var learnStartDate = new DateTime(2017, 1, 1);
+
+            var learner = new MessageLearner()
+            {
+                LearningDelivery = new MessageLearnerLearningDelivery[]
+                {
+                    new MessageLearnerLearningDelivery()
+                    {
+                        AimType = 1,
+                        ProgType = 1,
+                        FworkCode = 1,
+                        PwayCode = 1,
+                        LearnStartDate = learnStartDate
+                    },
+                    new MessageLearnerLearningDelivery()
+                    {
+                        AimType = 1,
+                        ProgType = 1,
+                        FworkCode = 1,
+                        PwayCode = 2,
+                    }
+                }
+            };
+
+            learner.EarliestLearningDeliveryLearnStartDateFor(1, 1, 1, 1).Should().Be(learnStartDate);
+        }
+
+        [Fact]
+        public void EarliestLearningDeliveryLearnStartDateFor_OrderedMatch()
+        {
+            var earliestLearnStartDate = new DateTime(2017, 1, 1);
+            var latestLearnStartDate = new DateTime(2018, 1, 1);
+
+            var learner = new MessageLearner()
+            {
+                LearningDelivery = new MessageLearnerLearningDelivery[]
+                {
+                    new MessageLearnerLearningDelivery()
+                    {
+                        AimType = 1,
+                        ProgType = 1,
+                        FworkCode = 1,
+                        PwayCode = 1,
+                        LearnStartDate = earliestLearnStartDate
+                    },
+                    new MessageLearnerLearningDelivery()
+                    {
+                        AimType = 1,
+                        ProgType = 1,
+                        FworkCode = 1,
+                        PwayCode = 1,
+                        LearnStartDate = latestLearnStartDate
+                    },
+                    new MessageLearnerLearningDelivery()
+                    {
+                        AimType = 1,
+                        ProgType = 1,
+                        FworkCode = 1,
+                        PwayCode = 2,
+                    }
+                }
+            };
+
+            learner.EarliestLearningDeliveryLearnStartDateFor(1, 1, 1, 1).Should().Be(earliestLearnStartDate);
+        }
+    }
+}
---
+++ src/ESFA.DC.ILR.ValidationService.Rules/Extensions/MessageLearnerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using ESFA.DC.ILR.Model;
+using System;
+using System.Linq;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Extensions
+{
+    public static class MessageLearnerExtensions
+    {
+        public static DateTime? EarliestLearningDeliveryLearnStartDateFor(this MessageLearner learner, long aimType, long progType, long fworkCode, long pwayCode)
+        {
+            if (learner.LearningDelivery == null)
+            {
+                return null;
+            }
+
+            return learner.LearningDelivery
+                .Where(ld => ld.AimType == aimType && ld.ProgType == progType && ld.FworkCode == fworkCode && ld.PwayCode == pwayCode)
+                .OrderBy(ld => ld.LearnStartDate)
+                .Select(ld => (DateTime?)ld.LearnStartDate)
+                .FirstOrDefault();
+        }
+    }
+}
