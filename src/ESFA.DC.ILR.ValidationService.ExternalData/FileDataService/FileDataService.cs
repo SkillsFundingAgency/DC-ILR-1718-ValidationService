1ae9c5b71bcd5c86f85624d315708b1ee727d4c8
Mike Childs <mike.childs@education.gov.uk>
2018-01-17 11:38:06 +0000

    External Data Project added, File Data Service Migrated with Test Project, XUnit nuget package updated

---
+++ src/ESFA.DC.ILR.ValidationService.ExternalData/FileDataService/FileDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ESFA.DC.ILR.Model;
+using ESFA.DC.ILR.ValidationService.ExternalData.FileDataService.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.ExternalData.FileDataService
+{
+    public class FileDataService : IFileDataService
+    {
+        public DateTime FilePreparationDate { get; private set; }
+
+        public void Populate(Message message)
+        {
+            FilePreparationDate = message.Header.CollectionDetails.FilePreparationDate;
+        }
+    }
+}
