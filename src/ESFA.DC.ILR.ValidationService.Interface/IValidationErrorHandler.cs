15bcb23dd144a71b7c9d170c8cec2e652604a708
Mike Childs <mike.childs@education.gov.uk>
2018-01-17 14:29:32 +0000

    Interface Project Added, Validation Error Handler Added, IRule Interface added, IReferenceDataCache added to External Data Project

---
+++ src/ESFA.DC.ILR.ValidationService.Interface/IValidationErrorHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.ILR.ValidationService.Interface
+{
+    public interface IValidationErrorHandler
+    {
+        void Handle(string ruleName, string learnRefNumer, long? aimSequenceNumber, IEnumerable<string> errorMessageParameters);
+    }
+}
---
+++ src/ESFA.DC.ILR.ValidationService.Service/ErrorHandler/ValidationErrorHandler.cs
@@ -0,0 +1,25 @@
+﻿using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Service.ErrorHandler.Model;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace ESFA.DC.ILR.ValidationService.Service.ErrorHandler
+{
+    public class ValidationErrorHandler : IValidationErrorHandler
+    {
+        private readonly ConcurrentBag<ValidationError> _errorbag = new ConcurrentBag<ValidationError>();
+
+        public ConcurrentBag<ValidationError> ErrorBag
+        {
+            get
+            {
+                return _errorbag;
+            }
+        }
+
+        public void Handle(string ruleName, string learnRefNumber = null, long? aimSequenceNumber = null, IEnumerable<string> errorMessageParameters = null)
+        {
+            _errorbag.Add(new ValidationError(ruleName, learnRefNumber, aimSequenceNumber, errorMessageParameters));
+        }
+    }
+}
