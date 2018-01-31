05eb0a9e82d7df334863d68a1111dd46eadf94df
Mike Childs <mike.childs@education.gov.uk>
2018-01-31 14:03:33 +0000

    Implemented Learner Model, Interface and Unit Tests

--- src/ESFA.DC.ILR.Model/Interface/IMessageLearner.cs
+++ src/ESFA.DC.ILR.Model/Interface/IMessageLearner.cs
@@ -5,14 +5,40 @@ namespace ESFA.DC.ILR.Model.Interface
 {
     public interface IMessageLearner
     {
+        long? AccomNullable { get; }
+        string AddLine1 { get; }
+        string AddLine2 { get; }
+        string AddLine3 { get; }
+        string AddLine4 { get; }
+        long? ALSCostNullable { get; }
         DateTime? DateOfBirthNullable { get; }
-
+        string Email { get; }
+        long? EthnicityNullable { get; }
+        string EngGrade { get; }
+        string FamilyName { get; }
+        string GivenNames { get; }
         string LearnRefNumber { get; }
+        long? LLDDHealthProbNullable { get; }
+        string MathGrade { get; }
         string NINumber { get; }
-        
-
+        long? PlanEEPHoursNullable { get; }
+        long? PlanLearnHoursNullable { get; }
+        long? PMUKPRNNullable { get; }
+        string Postcode { get; }
+        string PostcodePrior { get; }
+        string PrevLearnRefNumber { get; }
+        long? PrevUKPRNNullable { get; }
+        long? PriorAttainNullable { get; }
         long? ULNNullable { get; }
-        
+        string Sex { get; }
+        string TelNo { get; }
+
+        IReadOnlyCollection<IMessageLearnerContactPreference> ContactPreferences { get; }
+        IReadOnlyCollection<IMessageLearnerLearnerFAM> LearnerFAMs { get; }
         IReadOnlyCollection<IMessageLearnerLearningDelivery> LearningDeliveries { get; }
+        IReadOnlyCollection<IMessageLearnerLLDDAndHealthProblem> LLDDAndHealthProblems { get; }
+        IReadOnlyCollection<IMessageLearnerProviderSpecLearnerMonitoring> ProviderSpecLearnerMonitorings { get; }
+        IReadOnlyCollection<IMessageLearnerLearnerEmploymentStatus> LearnerEmploymentStatuses { get; }
+        IReadOnlyCollection<IMessageLearnerLearnerHE> LearnerHEs { get; }
     }
 }
--- src/ESFA.DC.ILR.Model/MessageLearner.cs
+++ src/ESFA.DC.ILR.Model/MessageLearner.cs
@@ -8,21 +8,63 @@ namespace ESFA.DC.ILR.Model
     public partial class MessageLearner : IMessageLearner
     {
         [XmlIgnore]
+        public long? AccomNullable
+        {
+            get { return accomFieldSpecified ? (long?)accomField : null; }
+        }
+        
+        [XmlIgnore]
+        public long? ALSCostNullable
+        {
+            get { return aLSCostFieldSpecified ? (long?)aLSCostField : null; }
+        }
+
+        [XmlIgnore]
         public DateTime? DateOfBirthNullable
         {
             get { return dateOfBirthFieldSpecified ? (DateTime?)dateOfBirthField : null; }
         }
 
         [XmlIgnore]
+        public long? EthnicityNullable
+        {
+            get { return ethnicityFieldSpecified ? (long?)ethnicityField : null; }
+        }
+
+        [XmlIgnore]
+        public long? LLDDHealthProbNullable
+        {
+            get { return lLDDHealthProbFieldSpecified ? (long?)lLDDHealthProbField : null; }
+        }
+
+        [XmlIgnore]
+        public long? PlanEEPHoursNullable
+        {
+            get { return planEEPHoursFieldSpecified ? (long?)planEEPHoursField : null; }
+        }
+
+        [XmlIgnore]
+        public long? PlanLearnHoursNullable
+        {
+            get { return planLearnHoursFieldSpecified ? (long?)planLearnHoursField : null; }
+        }
+        
+        [XmlIgnore]
         public long? PMUKPRNNullable
         {
-            get { return pMUKPRNFieldSpecified ? (long?)pMUKPRNField : null;  }
+            get { return pMUKPRNFieldSpecified ? (long?)pMUKPRNField : null; }
         }
 
         [XmlIgnore]
         public long? PrevUKPRNNullable
         {
-            get { return prevUKPRNFieldSpecified ? (long?)prevUKPRNField : null;  }
+            get { return prevUKPRNFieldSpecified ? (long?)prevUKPRNField : null; }
+        }
+
+        [XmlIgnore]
+        public long? PriorAttainNullable
+        {
+            get { return priorAttainFieldSpecified ? (long?)priorAttainField : null; }
         }
         
         [XmlIgnore]
@@ -32,9 +74,40 @@ namespace ESFA.DC.ILR.Model
         }
 
         [XmlIgnore]
+        public IReadOnlyCollection<IMessageLearnerContactPreference> ContactPreferences
+        {
+            get { return contactPreferenceField; }
+        }
+
+        public IReadOnlyCollection<IMessageLearnerLearnerEmploymentStatus> LearnerEmploymentStatuses
+        {
+            get { return learnerEmploymentStatusField; }
+        }
+
+        public IReadOnlyCollection<IMessageLearnerLearnerFAM> LearnerFAMs
+        {
+            get { return learnerFAMField; }
+        }
+
+        public IReadOnlyCollection<IMessageLearnerLearnerHE> LearnerHEs
+        {
+            get { return learnerHEField; }
+        }
+
+        [XmlIgnore]
         public IReadOnlyCollection<IMessageLearnerLearningDelivery> LearningDeliveries
         {
             get { return learningDeliveryField; }
+        }
+
+        public IReadOnlyCollection<IMessageLearnerLLDDAndHealthProblem> LLDDAndHealthProblems
+        {
+            get { return lLDDandHealthProblemField; }
+        }
+
+        public IReadOnlyCollection<IMessageLearnerProviderSpecLearnerMonitoring> ProviderSpecLearnerMonitorings
+        {
+            get { return providerSpecLearnerMonitoringField; }
         }
     }
 }
