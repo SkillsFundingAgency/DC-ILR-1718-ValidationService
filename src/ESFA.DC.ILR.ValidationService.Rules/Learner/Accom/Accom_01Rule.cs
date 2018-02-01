f133c89ad7b9da135edddf9185515c390ab3de9d
Salman Baig <salman.sfa@outlook.com>
2018-02-01 15:08:10 +0000

    changes to add Accom rule

---
+++ src/ESFA.DC.ILR.ValidationService.Rules/Learner/Accom/Accom_01Rule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Abstract;
+using ESFA.DC.ILR.ValidationService.Rules.Query.Interface;
+
+namespace ESFA.DC.ILR.ValidationService.Rules.Learner.Accom
+{
+    public class Accom_01Rule :  AbstractRule, IRule<IMessageLearner>
+    {
+        public Accom_01Rule(IValidationErrorHandler validationErrorHandler)
+            : base(validationErrorHandler)
+        {
+            
+        }
+
+        public void Validate(IMessageLearner objectToValidate)
+        {
+            
+            if (ConditionMet(objectToValidate.AccomNullable))
+            {
+                HandleValidationError(RuleNameConstants.Accom_01Rule, objectToValidate.LearnRefNumber, null);
+            }
+            
+
+        }
+
+
+        public bool ConditionMet(long? accomValue)
+        {
+            return accomValue.HasValue && accomValue.Value != 5;
+
+        }
+    }
+}
