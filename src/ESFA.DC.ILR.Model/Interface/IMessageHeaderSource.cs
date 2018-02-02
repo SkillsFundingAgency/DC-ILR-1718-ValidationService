f84202dc41518b8ea7c2a8b872dcf77b7c8a86b4
Mike Childs <mike.childs@education.gov.uk>
2018-02-02 17:21:57 +0000

    Message Header Source Model Partial Class, Interface and Unit Tests Implemented

--- src/ESFA.DC.ILR.Model/Interface/IMessageHeaderSource.cs
+++ src/ESFA.DC.ILR.Model/Interface/IMessageHeaderSource.cs
@@ -1,6 +1,17 @@
-﻿namespace ESFA.DC.ILR.Model.Interface
+﻿using System;
+
+namespace ESFA.DC.ILR.Model.Interface
 {
     public interface IMessageHeaderSource
     {
+        string ProtectiveMarkingString { get; }
+        int UKPRN { get; }
+        string SoftwareSupplier { get; }
+        string SoftwarePackage { get; }
+        string Release { get; }
+        string SerialNo { get; }
+        DateTime DateTime { get; }
+        string ReferenceData { get; }
+        string ComponentSetVersion { get; }
     }
 }
--- src/ESFA.DC.ILR.Model/MessageHeaderSource.cs
+++ src/ESFA.DC.ILR.Model/MessageHeaderSource.cs
@@ -1,8 +1,13 @@
-﻿using ESFA.DC.ILR.Model.Interface;
+﻿using ESFA.DC.ILR.Model.Extension;
+using ESFA.DC.ILR.Model.Interface;
 
 namespace ESFA.DC.ILR.Model
 {
     public partial class MessageHeaderSource : IMessageHeaderSource
     {
+        public string ProtectiveMarkingString
+        {
+            get { return protectiveMarkingField.XmlEnumToString(); }
+        }
     }
 }
