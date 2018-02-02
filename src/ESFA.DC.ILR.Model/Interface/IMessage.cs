4023a485b0206f4e465b74b6194cdaffde3e7a66
Mike Childs <mike.childs@education.gov.uk>
2018-02-02 14:55:35 +0000

    Message Partial Class, Interface and Unit Tests Implemented

---
+++ src/ESFA.DC.ILR.Model/Interface/IMessage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.ILR.Model.Interface
+{
+    public interface IMessage
+    {
+        IMessageHeader HeaderEntity { get; }        
+        IReadOnlyCollection<IMessageSourceFile> SourceFilesCollection { get; }
+        IMessageLearningProvider LearningProviderEntity { get; }
+        IReadOnlyCollection<IMessageLearner> Learners { get; }
+        IReadOnlyCollection<IMessageLearnerDestinationAndProgression> LearnerDestinationAndProgressions { get; }        
+    }
+}
