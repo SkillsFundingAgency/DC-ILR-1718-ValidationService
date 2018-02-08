29aeacdb043ffc2d51b310100d91de45e398d776
Mike Childs <mike.childs@education.gov.uk>
2018-02-08 15:06:20 +0000

    Removing Reference to ILR Model Project and Unit Tests

--- src/ESFA.DC.ILR.ValidationService.ExternalData/FileDataService/Interface/IFileDataService.cs
+++ src/ESFA.DC.ILR.ValidationService.ExternalData/FileDataService/Interface/IFileDataService.cs
@@ -1,4 +1,4 @@
-﻿using ESFA.DC.ILR.Model;
+﻿using ESFA.DC.ILR.Model.Interface;
 using System;
 
 namespace ESFA.DC.ILR.ValidationService.ExternalData.FileDataService.Interface
@@ -7,6 +7,6 @@ namespace ESFA.DC.ILR.ValidationService.ExternalData.FileDataService.Interface
     {
         DateTime FilePreparationDate { get; }
 
-        void Populate(Message message);
+        void Populate(IMessage message);
     }
 }
