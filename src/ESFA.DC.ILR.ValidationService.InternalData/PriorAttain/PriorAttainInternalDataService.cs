963dcb4682f1d94e44478ef91c9e3a6ca37a2fea
Salman Baig <salman.sfa@outlook.com>
2018-02-15 14:04:42 +0000

    marked hashset readonly

--- src/ESFA.DC.ILR.ValidationService.InternalData/PriorAttain/PriorAttainInternalDataService.cs
+++ src/ESFA.DC.ILR.ValidationService.InternalData/PriorAttain/PriorAttainInternalDataService.cs
@@ -4,7 +4,7 @@ namespace ESFA.DC.ILR.ValidationService.InternalData.PriorAttain
 {
     public class PriorAttainInternalDataService : IPriorAttainInternalDataService
     {
-        private HashSet<long> _validPriorAttainLookupValues = new HashSet<long> { 2, 3, 4, 5, 10, 11, 12, 13, 97, 98 };
+        private readonly HashSet<long> _validPriorAttainLookupValues = new HashSet<long> { 2, 3, 4, 5, 10, 11, 12, 13, 97, 98 };
 
         public bool Exists(long priorAttain)
         {
