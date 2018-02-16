0f9f6d6f0ac9ac7bbf169735d86245b8eafc93a3
Mike Childs <mike.childs@education.gov.uk>
2018-02-16 15:35:06 +0000

    RuleSet Module Project added, Unit Tests

---
+++ src/ESFA.DC.ILR.ValidationService.RuleSet.Modules/ConsoleRuleSetModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using ESFA.DC.ILR.Model.Interface;
+using ESFA.DC.ILR.ValidationService.Interface;
+using ESFA.DC.ILR.ValidationService.Rules.Learner.Accom;
+
+namespace ESFA.DC.ILR.ValidationService.RuleSet.Modules
+{
+    public class ConsoleRuleSetModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Accom_01Rule>().As<IRule<ILearner>>();
+        }
+    }
+}
